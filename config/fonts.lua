5cb8da77b2aeaa87709a953e4410b1fe798c6f61
H.N <hun1982@qq.com>
2024-08-11 01:22:13 +0200

    202408110122

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,16 +1,23 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'JetBrains Mono'
+local font1 = 'JetBrains Mono'
+local font2 = 'Nerd Font'
+local font3 = 'Agave Nerd Font'
+local font4 = 'FiraCode Nerd Font'
+
 local font_size = platform().is_mac and 12 or 16
 
 return {
    font = wezterm.font_with_fallback({
-      font,
-      'FiraCode Nerd Font',
-      'Noto Sans Mono',
+      font1,
+      font2,
+      font3,
+      font4,
+      -- 'Noto Sans Mono',
    }),
    font_size = font_size,
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
    freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+   warn_about_missing_glyphs = false,
 }
